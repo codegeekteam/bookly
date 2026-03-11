a1d87f3762de5aa74da0d446fe005eb52189617d
Sreeja <sreeja.bs@gmail.com>
2026-03-11 12:31:15 +0530

    validation rule interface

--- app/Rules/ValidSlot.php
+++ app/Rules/ValidSlot.php
@@ -3,34 +3,35 @@
 namespace App\Rules;
 
 use Closure;
-use Illuminate\Contracts\Validation\ValidationRule;
-use Illuminate\Validation\Rule;
 
-class ValidSlot implements Rule //ValidationRule
+use Illuminate\Contracts\Validation\ValidationRule;
+
+
+
+class ValidSlot implements ValidationRule
 {
     /**
      * Run the validation rule.
      *
      * @param  \Closure(string): \Illuminate\Translation\PotentiallyTranslatedString  $fail
      */
-    // public function validate(string $attribute, mixed $value, Closure $fail): void
-    // {
-    //     //
-    // }
-
-    public function passes($attribute, $value)
+    public function validate(string $attribute, mixed $value, Closure $fail): void
     {
         $data = json_decode($value, true);
 
         if (!is_array($data)) {
-            return false;
+            $fail("The $attribute must be valid JSON.");
+            return;
         }
 
-        return isset($data['service_id'], $data['timeslot']);
-    }
+        if (!isset($data['service_id'])) {
+            $fail("service_id is required in $attribute.");
+        }
 
-    public function message()
-    {
-        return 'Each slot must be valid JSON containing service_id and timeslot.';
-    }
+        if (!isset($data['timeslot'])) {
+            $fail("timeslot is required in $attribute.");
+        }
+    } 
+
+   
 }
