f70283fe0b2de37a6d6a510696fdbdfd18f86724
Sreeja <sreeja.bs@gmail.com>
2026-03-11 12:25:12 +0530

    validation rule change

--- app/Http/Requests/RescheduleAppointmentCustomerRequest.php
+++ app/Http/Requests/RescheduleAppointmentCustomerRequest.php
@@ -2,10 +2,30 @@
 
 namespace App\Http\Requests;
 
+use App\Rules\ValidSlot;
 use Illuminate\Foundation\Http\FormRequest;
 
 class RescheduleAppointmentCustomerRequest extends FormRequest
 {
+
+    // protected function prepareForValidation()
+    // {
+    //     $decodedSlots = [];
+
+    //     if (is_array($this->slots)) {
+    //         foreach ($this->slots as $slot) {
+    //             $decoded = json_decode($slot, true);
+
+    //             if (json_last_error() === JSON_ERROR_NONE) {
+    //                 $decodedSlots[] = $decoded;
+    //             }
+    //         }
+    //     }
+
+    //     $this->merge([
+    //         'slots' => $decodedSlots
+    //     ]);
+    // }
     /**
      * Get the validation rules that apply to the request.
      *
@@ -15,7 +35,10 @@ class RescheduleAppointmentCustomerRequest extends FormRequest
     {
         return [
             'appointment_id' => 'required|exists:appointments,id',
-            'slots' => 'required|array|min:1',   
+            'slots' => 'required|array',
+            'slots.*' => ['required', 'string', new ValidSlot],
+            // 'slots.*.service_id' => 'required|integer',
+            // 'slots.*.timeslot' => 'required|string',   
             'date' => 'required',
             'employee_id' => 'nullable',
         ];
