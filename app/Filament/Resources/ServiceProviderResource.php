d770b859be53c46e8636ab3a88612dca304a2f0f
Sreeja-b-s <sreeja.bs@gmail.com>
2026-05-25 14:37:58 +0530

    admin approve sp

--- app/Filament/Resources/ServiceProviderResource.php
+++ app/Filament/Resources/ServiceProviderResource.php
@@ -84,13 +84,18 @@ class ServiceProviderResource extends Resource
                             ->placeholder(__('Select Provider Type'))
                             ->preload()
                             ->searchable(),
+                      Toggle::make('is_active')
+                            ->label(__('Approved / Active')),
+                          /*    ->helperText(__('Activate provider after admin approval'))
+                            ->default(false)
+                            ->visible(fn ($record) => !$record || !$record->is_active)
+                            ->disabled(fn ($record) => $record?->is_active),*/
                         Toggle::make('is_blocked')
-                            ->label(__('Blocked')),
+                            ->label(__('Blocked'))
+                            ->visible(fn ($record) => $record?->is_active),                                              
                         Toggle::make('published')
                             ->label(__('Published'))
-                            ->default(true),
-                        /*Toggle::make('is_active')
-                            ->label(__('Active')),*/
+                            ->default(true),                    
                     ])->columns(2),
 
                 Section::make(__('Legal Details'))
