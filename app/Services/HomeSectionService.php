02f63f5cb455a604f7125d7634ff0a86d5c88481
Sreeja-b-s <sreeja.bs@gmail.com>
2026-06-05 15:48:21 +0530

    query time get

--- app/Services/HomeSectionService.php
+++ app/Services/HomeSectionService.php
@@ -4,11 +4,13 @@ namespace App\Services;
 
 use App\Http\Resources\HomeSectionResource;
 use App\Models\HomeSection;
+use Illuminate\Support\Facades\DB;
 
 class HomeSectionService
 {
     public function index()
     {
+        DB::enableQueryLog();
         $sections=HomeSection::with(
             [   'providers' => function ($q) {
                     $q->withAvg('reviews', 'rate');
@@ -18,10 +20,10 @@ class HomeSectionService
                 'providers.operationalHours',     
                 'providers.reviews',
                 'providers.user.activeSubscription',
-                'providers.addresses',
-                'providers.user.activeSubscription',
+               // 'providers.addresses',
             ])
             ->get();
+            dd(DB::getQueryLog());
         return HomeSectionResource::collection($sections);
     }
 
