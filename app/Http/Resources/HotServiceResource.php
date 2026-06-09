f5381e4b6a886c65b8373b45b0f6e34cbcea8387
Sreeja-b-s <sreeja.bs@gmail.com>
2026-06-09 13:13:13 +0530

    full resource

--- app/Http/Resources/HotServiceResource.php
+++ app/Http/Resources/HotServiceResource.php
@@ -14,9 +14,9 @@ class HotServiceResource extends JsonResource
             'id' => $this->id,
             'title' => $this->getTranslation('title', $request->header('lang') ?? 'en'),
             'is_active' => $this->is_active,
-          /*  'description' => $this->description,
+            'description' => $this->description,
             'average_price' => $this->attachedServices->avg('price'),
-            'currency' => 'SAR',*/
+            'currency' => 'SAR',
             'min_price' => floatval($this->attachedServices->min('price')),
             'max_price' => floatval($this->attachedServices->max('price')),
             'image' => $this->categories->first()?->getFirstMediaUrl('category_images') ? $this->categories->first()?->getMedia('category_images')?->last()?->getUrl() : asset('assets/default.jpg'), // TODO: add ability to add service photo in the dashboard and change the collection accordingly
