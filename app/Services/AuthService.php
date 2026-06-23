212cb6b40ce26a2524c139bd0aab661a6befc5d6
Sreeja-b-s <sreeja.bs@gmail.com>
2026-06-23 11:45:07 +0530

    restored sp inactive login

--- app/Services/AuthService.php
+++ app/Services/AuthService.php
@@ -38,9 +38,9 @@ class AuthService extends Service
             });   
             return __('Your signup request has been received. Our team will review your details and contact you soon to activate your account');
         }
-        // elseif($access_type == 'provider' && $account->is_active == 0 && $create_flag == false) {  //SP login attempt
-        //       return __('Account Pending Approval');
-        // }
+        elseif($access_type == 'provider' && $account->is_active == 0 && $create_flag == false) {  //SP login attempt
+              return __('Account Pending Approval');
+        }
         else{
             $this->SendOTP($account);
             return 'OTP sent';
